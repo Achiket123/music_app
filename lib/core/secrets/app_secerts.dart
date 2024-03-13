6b49d4b88dcc4e438a9960d8816a2e6434aa1822
Achiket Kumar <126181868+Achiket123@users.noreply.github.com>
2024-03-14 02:54:17 +0530

    Update readme.md

--- lib/core/secrets/app_secerts.dart
+++ lib/core/secrets/app_secerts.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://vndfsnhswwcrcowaglqc.supabase.co';
+  static const supabaseUrl = '';
   static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuZGZzbmhzd3djcmNvd2FnbHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNTUwMjMsImV4cCI6MjAyNTkzMTAyM30.NFKTrMVXtxtrL9J_OKs4pDE6w1cNVnYuMlE633xt0gU';
+      '';
 }
